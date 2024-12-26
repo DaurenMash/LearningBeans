1a2943dac4a92972af87e1c57e4026a3ced4f9cd
DaurenMash <doxa@inbox.ru>
2024-12-26 10:20:34 +0300

    Cat class

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,22 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
+
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld beanUno  =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(beanUno.getMessage());
+
+        Cat koshka = (Cat) applicationContext.getBean("cat");
+        System.out.println(koshka.getName());
+
+        Cat koshkaUno = (Cat) applicationContext.getBean("cat");
+        System.out.println(koshkaUno.getName());
+
+        System.out.println(bean == beanUno);
+        System.out.println(koshka == koshkaUno);
     }
 }
