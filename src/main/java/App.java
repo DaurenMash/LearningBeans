49011934304c76620bed622f95ab41ee84458625
DaurenMash <doxa@inbox.ru>
2024-12-26 10:32:48 +0300

    Cat class

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,17 +8,15 @@ public class App {
 
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
 
         HelloWorld beanUno  =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(beanUno.getMessage());
 
         Cat koshka = (Cat) applicationContext.getBean("cat");
-        System.out.println(koshka.getName());
 
         Cat koshkaUno = (Cat) applicationContext.getBean("cat");
-        System.out.println(koshkaUno.getName());
+
+
 
         System.out.println(bean == beanUno);
         System.out.println(koshka == koshkaUno);
